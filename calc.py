5eec5b2c5e56da57d674ca5e3ad599ea4d55040f
s99ss <alghilanisuhaila@gmail.com>
2025-04-21 10:41:31 +0400

    calc !

--- calc.py
+++ calc.py
@@ -1,6 +1,6 @@
 num_1 = int(input("enter 1st number"))
 num_2 = int(input("enter 2nd number"))
-operation = input("select + or - or * or / or **:")
+operation = input("select + or - or * or / or ** or !:")
 result = 0
 '''
 print(num_1)
@@ -17,5 +17,10 @@ elif operation== "/" :
     result = num_1 / num_2
 elif operation== "**" :
     result = num_1 ** num_2
+elif operation== "!" :
+    result=1
+
+    for i in range(1, num_1+1):
+        result=result*i
 
 print (" result is : " , result )
