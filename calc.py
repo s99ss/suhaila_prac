cb42cd913d3578e8753bfc854dd0653625cceb0b
s99ss <alghilanisuhaila@gmail.com>
2025-04-21 15:19:46 +0400

    calc OR

--- calc.py
+++ calc.py
@@ -1,6 +1,6 @@
 num_1 = int(input("enter 1st number"))
 num_2 = int(input("enter 2nd number"))
-operation = input("select + or - or * or / or ** or ! or absolute:")
+operation = input("select + or - or * or / or ** or ! or absolute or (OR) |:")
 result = 0
 '''
 print(num_1)
@@ -26,5 +26,8 @@ elif operation== "!" :
 
 elif operation== "absolute":
     result= abs(num_1)
+
+elif operation == "|":
+     result = num_1 | num_2
 
 print (" result is : " , result )
