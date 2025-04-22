942999e67182d95411136f237e42c3de2bbea564
s99ss <alghilanisuhaila@gmail.com>
2025-04-22 13:40:14 +0400

    Done with divison functionality

--- practise.py
+++ practise.py
@@ -1,6 +1,6 @@
 num_1 = int(input("enter 1st number"))
 num_2 = int(input("enter 2nd number"))
-operation = input("select + or - :")
+operation = input("select +  - or / :")
 result = 0
 '''
 print(num_1)
@@ -11,5 +11,6 @@ if operation== "+" :
     result = num_1 + num_2
 elif operation== "-" :
     result = num_1 - num_2
-
+elif operation== "/" :
+    result = num_1 / num_2
 print (" result is : " , result )
