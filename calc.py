7cf9e44a0171b5c28f76c43c0c4fe04b169e17f0
Syed Atyab Hussain <syed.atyab.hussain.om@gmail.com>
2025-04-23 16:00:03 +0400

    Added a pow method

--- calc.py
+++ calc.py
@@ -1,6 +1,8 @@
+import math
+
 num_1 = int(input("enter 1st number"))
 num_2 = int(input("enter 2nd number"))
-operation = input("select + or - or * or / or ** or ! or absolute or (OR) | or &:")
+operation = input("select + or - or * or / or ** or ! or absolute or (OR) | or & or ^:")
 result = 0
 '''
 print(num_1)
@@ -32,5 +34,7 @@ elif operation == "|":
 
 elif operation == "&":
      result = num_1 & num_2
+elif operation == "^":
+    result = math.pow(num_1, num_2)
 
 print (" result is : " , result )
