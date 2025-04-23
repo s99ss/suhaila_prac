c35c6952877d748eb0bf5f38e575613b10bc1ccf
HananBlc9 <hanan.balucci@gmail.com>
2025-04-23 15:04:54 +0400

    Added modulus functionality

--- calc_practise.py
+++ calc_practise.py
@@ -1,8 +1,11 @@
 num_1 = int(input("enter 1st number"))
 num_2 = int(input("enter 2nd number"))
-operation = input("select + or - or * or / or ** or ! or absolute or (OR) | or &:")
+operation = input("select + or - or * or / or ** or ! or absolute or (OR) | or & or %:")
 result = 0
+if operation == "%":
+    result = num_1 % num_2
 
 print(num_1)
 print(num_2)
+print(result)
 
